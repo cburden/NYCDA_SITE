5eee78d5d62ca12fa285dc4d3d14c19b52fbf776
Casey Burden <caseyburden@me.com>
2014-09-04 16:51:42 -0400

    Fix Alumni Data

--- javascript/alumniApp.js
+++ javascript/alumniApp.js
@@ -2,14 +2,29 @@
 
 (function(){
 	
-	var app = angular.module('alumniApp', ['nycdaApp', 'alumniData']);
+	var app = angular.module('alumniApp', ['nycdaApp']);
 	
+	app.factory('alumniData', function($http){
+		
+		return {
+			'alumn' : function(){
+				return $http.get('/api/alumni');
+			}
+		};
+	
+	});
+
+
 	app.directive('alumniList', function(){
 		return {
 			restrict: 'E',
 			templateUrl: '../partials/alumniBioList.html',
 			controller: function($scope, alumniList, $sce){
-				$scope.alumni = alumniList.alumn();
+
+				alumniList.alumn()
+					.success(function(data){
+						$scope.alumni = alumniList.alumn();
+					});
 
 			}
 		};
